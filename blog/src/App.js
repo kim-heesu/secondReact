7f050e3ac3ce6660576e48aa3a43f5ab721cc8be
kim-heesu <cjmgmltn@naver.com>
2023-08-13 23:25:20 +0900

    state수정 얕은복사,깊은복사 sort로 가나다순정렬하기

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,14 +1,70 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  let post ='강남 우동 맛집'
+
+  let [logo,setLogo] = useState('Reactblog');
+
+  let [title01,setTitle01] = useState('남자코트추천');
+  let [title02,settitle02] = useState('강남우동맛집');
+  let [title03,settitle03] = useState('파이썬독학');
+  
+  let [titleTest,setTitleTest] = useState(['남자코트추천','강남우동맛집','파이썬독학'])
+  let [like, setLike] = useState(0);
+
+  function likePlus(){
+    setLike(like+1);
+  }
+
+  function nameChange(){
+    setTitle01('여자코트추천')
+  }
+
+  function titleChange(){
+    let copy = [...titleTest];
+    // state변경 함수는 기존 스테이트, 신규 스테이트가 같다면 변경하지않는다.
+    copy[0] ='여자코트추천'
+    setTitleTest(copy)
+  }
+  
+  function titleSort(){
+    let sortCopy =[...titleTest];
+    sortCopy.sort();
+    setTitleTest(sortCopy)
+  }
+
+  // Destructuring문법
+  let num = [1,2];
+  let [a,c] = [1,2];// a = 1 , b = 2  가 된다.
+  
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{color:"red", fontSize:"15px"}}>블로그테스트</h4>
+        <h4 style={{color:"red", fontSize:"15px"}}>{logo}</h4>
       </div>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{title01} <span onClick={likePlus}>좋아요</span> {like} </h4>
+        <button onClick={nameChange}>이름바뀜</button>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title02}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title03}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <button onClick={titleSort}>가나다순정렬</button>
+      <div className='list'>
+        <h4>{titleTest[0]}</h4>
+        <h4>{titleTest[1]}</h4>
+        <h4>{titleTest[2]}</h4>
+        <button onClick={titleChange}>버튼버튼버튼</button>
+      </div>
     </div>
   );
 }
