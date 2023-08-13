936ed81f85dccb251ec036d1c81381cf041d1a1b
kim-heesu <cjmgmltn@naver.com>
2023-08-13 21:28:28 +0900

    jsx문법 1. class => className 2. style {{}} 3. 데이터바인딩 {}

---
+++ blog/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let post ='강남 우동 맛집'
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4 style={{color:"red", fontSize:"15px"}}>블로그테스트</h4>
+      </div>
+      <h4>{post}</h4>
+    </div>
+  );
+}
+
+export default App;
