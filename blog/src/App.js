81a38048345fd3a69b35c3afaeb26641bc6d52cf
kim-heesu <cjmgmltn@naver.com>
2023-08-15 02:47:59 +0900

    jsx문법에서는 onClick={likePlus()} 와 같이 작성시 해당 함수가 즉시 실행된다.

--- blog/src/App.js
+++ blog/src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   let [logo,setLogo] = useState('Reactblog');
   
-  let [titleTest,setTitleTest] = useState(['리액트재밌다','뷰가재밌다','앵귤러는??'])
+  let [titleTest,setTitleTest] = useState(['남자코트추천','강남우동맛집','파이썬독학'])
   let [like, setLike] = useState([0,15,0]);
 
   let [modal,setModal] = useState(false);
@@ -19,7 +19,7 @@ function App() {
   }
 
   function nameChange(){
-    setTitle01('리액트잼있다')
+    setTitle01('여자코트추천')
   }
 
   function titleChange(){
@@ -49,15 +49,15 @@ function App() {
         titleTest.map(function(a,i){
           return <div className="list" id={"list"+i}>
           <h4>
-            {titleTest[i]} 
-            <span onClick={()=>likePlus(i)}>좋아요</span>
+            {titleTest[i]}  
+            <span onClick={()=>likePlus(i)} style={{"margin":"0 5px"}}>좋아요</span>
             {like[i]}
           </h4>
           <p>2월 17일 발행</p>
         </div>
         })
       }
-      <button onClick={titleChange}>버튼버튼버튼</button>
+      <button onClick={titleChange}>이름바꾸기</button>
       <button onClick={titleSort}>가나다순정렬</button>
 
       <button onClick={()=>{modal === false ? setModal(true) : setModal(false)}}>toggleModal</button>
