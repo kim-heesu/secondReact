7c965261ebcd435a80c3b4a1f65f14412d8d823c
kim-heesu <cjmgmltn@naver.com>
2023-08-17 11:47:04 +0900

    props방법 1. <자식컴포넌트 이름={state이름}> 2. props파라메터 등록 후 props.이름 사용 자식 > 부모로만 전송, 형제끼리는 전송 안됨

--- blog/src/App.js
+++ blog/src/App.js
@@ -18,10 +18,6 @@ function App() {
     setLike(likeCopy);
   }
 
-  function nameChange(){
-    setTitle01('여자코트추천')
-  }
-
   function titleChange(){
     let copy = [...titleTest];
     // state변경 함수는 기존 스테이트, 신규 스테이트가 같다면 변경하지않는다.
@@ -57,27 +53,34 @@ function App() {
         </div>
         })
       }
-      <button onClick={titleChange}>이름바꾸기</button>
+      <buttoln onClick={titleChange}>이름바꾸기</buttoln>
       <button onClick={titleSort}>가나다순정렬</button>
 
       <button onClick={()=>{modal === false ? setModal(true) : setModal(false)}}>toggleModal</button>
       
       {
-        modal === true ? <Modal/> : null
+        modal === true ? <Modal change={titleChange} color="pink" titleTest={titleTest}/> : null
       } 
       
+      {/* props방법
+      1. <자식컴포넌트 이름={state이름}>
+      2. props파라메터 등록 후 props.이름 사용
+      
+      자식 > 부모로만 전송, 형제끼리는 전송 안됨
+      */}
 
     </div>
   );
 }
 
 // 컴포넌트 만드는 방법1
-function Modal(){
+function Modal(props){
   return(
-    <div className="modal">
-      <h4>제목</h4>
+    <div className="modal" style={{background: props.color}}>
+      <h4>{props.titleTest[0]}</h4>
       <p>날짜</p>
       <p>내용</p>
+      <button onClick={props.change}>글 수정</button>
     </div>
   )
 }
