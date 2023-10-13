fe43bb455c7e555e7ec060e3d353fdc85f5aafb6
kim-heesu <cjmgmltn@naver.com>
2023-10-13 18:47:43 +0900

    styled component 사용방법, 장점과 단점

--- shop/src/routes/Detail.jsx
+++ shop/src/routes/Detail.jsx
@@ -1,5 +1,38 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+// styled component 사용방법
+let YellowButton = styled.button`
+  background: yellow;
+  color:black;
+  padding:10px;
+`
+// 비슷한데 살짝만 다른 스타일은 props를 사용하자
+// if문으로 받은 props값에 따라 다른 스타일을 줄수도있다!
+let BasicButton = styled.button`
+  background: ${props => props.bg};
+  color: ${props => props.bg  == 'blue' ? 'white' : '#000' };
+  padding:10px;
+`
+
+// BasicButton컴포넌트의 스타일을 그대로 가져와서 특정부분만 추가하여 사용할 수도 있다.
+// 스타일을 상속받은 BasicButton과 동일한 태그를 가진 컴포넌트가 생성된다.
+let NewBasicBtn = styled(BasicButton)`
+  border: 1px solid red;
+`
+
+
+let BlackBox = styled.div`
+  background:#000;
+  color:#fff;
+  padding: 20px;
+`
+
+// styled component의 단점은?
+// 1. js 파일 복잡해짐
+// 2. 스타일 재사용 할시에는 import 해서 쓰면되지만, 이건 걍 css쓰면 됨
+// 3. 협업시 css 담당의 숙련도 이슈
 
 function Detail(props){
   let params = useParams();
@@ -18,6 +51,18 @@ function Detail(props){
           <p>{props.product[thisId].price}원</p>
           <p>{}</p>
           <button className="btn btn-danger">주문하기</button>
+
+          <BlackBox>
+            
+            <YellowButton>스타일컴포넌트 만드는방법 let 컴포넌트이름 = styled.태그이름`스타일작성`</YellowButton>
+
+            {/* 동일한 컴포넌트에 props로 bg값을 전달하여 사용 */}
+            <BasicButton bg="blue">bg : blue button</BasicButton>
+            <BasicButton bg="yellow">bg : yellow button</BasicButton>
+
+            {/* BasicButton컴포넌트의 스타일을 그대로 가져와서 특정부분만 추가하여 사용할 수도 있다. */}
+            <NewBasicBtn bg="blue">border: 1px solid red 만 추가햇어요~</NewBasicBtn>
+          </BlackBox>
         </div>
       </div>
     </>
